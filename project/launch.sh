926ccf89f0aa2fe919b6aaedb7fdc1ac86bc11e3
Julius Piso <julius@piso.at>
2022-08-30 22:06:29 +0200

    Cleanup

--- project/launch.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-if [[ ! -z "$1" ]]; then
-    docker run -v $(pwd):/mnt/output -it docker-viennaps "/mnt/output/$1"
-else
-    echo "Please provide the name of the executable as argument."
-fi
